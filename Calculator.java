87bc66a3f43f1a3c94b493e55527426b7d442b0a
mahall05 <hallmatthewd12@gmail.com>
2022-08-01 20:04:17 -0400

    somuchprogress

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,12 @@
+import java.lang.Math;
+import java.util.Scanner;
 
 public class Calculator {
-    public static char[] ops = {'*', '/', '+', '-'};
+    public static char[] ops = {'^', '*', '/', '+', '-'};
+    public Scanner in = new Scanner(System.in);
+
+    public static void input(){
+        System.out.print("Enter operation >> ");
+        
+    }
 }
