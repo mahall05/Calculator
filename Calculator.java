9290848ac47bf2e94eb743a250dd17d0063e0307
Matt <hallmatthewd12@gmail.com>
2022-08-03 11:10:39 -0400

    Works with parentheses

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-    private static char[] ops = {'^', '*', '/', '+', '-'};
+    private static char[] ops = {'(', ')', '^', '*', '/', '+', '-'};
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
@@ -19,56 +19,107 @@ public class Calculator {
             }
 
             if(solving){
-                int pos = -1;
+                System.out.println("Result: " + evaluate(equation, false));
+            }
+        }
+    }
 
-                // EXPONENTS
-                for(int i = 0; i < equation.length(); i++){ // Search the entire equation for ^
-                    if(equation.charAt(i) == '^'){
-                        pos = i;
+    public static String evaluate(String equation, boolean p){
+        int pos = -1;
+
+        // PARENTHESES
+        {
+            int openPos = -1, closePos = -1;
+            int openNum = 0, closeNum = 0;
+            for(int i = 0; i < equation.length(); i++){
+                if(equation.charAt(i) == '('){
+                    openNum++;
+                    if(openNum == 1){
+                        openPos = i;
                     }
-
-                    if(pos > -1){
-                        equation = replace(equation, findRange(equation, pos), solve(findSet(equation, pos)));
-                        System.out.println(equation);
-                        i = 0; // Added this because shorting the equation would cause i to be out of range
+                }else if(equation.charAt(i) == ')'){
+                    closeNum++;
+                    if(closeNum == openNum){
+                        closePos = i;
                     }
-
-                    pos = -1;
                 }
+                int[] parenRange = {openPos+1, closePos-1};
 
-                // MULTIPLICATION / DIVISION
-                for(int i = 0; i < equation.length(); i++){
-                    if(equation.charAt(i) == '*' || equation.charAt(i) == '/'){
-                        pos = i;
+                if(closePos > -1){
+                    boolean operation = false;
+                    for(int j = openPos+1; j < closePos; j++){
+                        if(!Character.isDigit(equation.charAt(j))){
+                            operation = true;
+                            j = closePos+100;
+                        }
                     }
-
-                    if(pos > -1){
-                        equation = replace(equation, findRange(equation, pos), solve(findSet(equation, pos)));
+                    if(operation){
+                        equation = replace(equation, parenRange, evaluate(equation.substring(openPos+1, closePos), true));
                         System.out.println(equation);
-                        i = 0;
                     }
-
-                    pos = -1;
-                }
-
-                // ADDITION / SUBTRACTION
-                for(int i = 1; i < equation.length(); i++){
-                    if(equation.charAt(i) == '+' || equation.charAt(i) == '-'){
-                        pos = i;
-                    }
-
-                    if(pos > -1){
-                        equation = replace(equation, findRange(equation, pos), solve(findSet(equation, pos)));
-                        System.out.println(equation);
-                        i = 0;
-                    }
-
-                    pos = -1;
+                    openPos = -1;
+                    closePos = -1;
+                    openNum = 0;
+                    closeNum = 0;
                 }
             }
         }
 
-        // END SOLVING
+        // EXPONENTS
+        for(int i = 0; i < equation.length(); i++){ // Search the entire equation for ^
+            if(equation.charAt(i) == '^'){
+                pos = i;
+            }
+
+            if(pos > -1){
+                equation = replace(equation, findRange(equation, pos), solve(findSet(equation, pos), '^'));
+                if(!p)
+                    System.out.println(equation);
+                i = 0; // Added this because shorting the equation would cause i to be out of range
+            }
+
+            pos = -1;
+        }
+
+        // MULTIPLICATION / DIVISION
+        for(int i = 1; i < equation.length(); i++){
+            if(equation.charAt(i) == '*' || equation.charAt(i) == '/'){
+                pos = i;
+            }
+
+            if(equation.charAt(i) == '(' || equation.charAt(i) == ')'){
+                if(i < equation.length()-1 && Character.isDigit(equation.charAt(i-1)) && Character.isDigit(equation.charAt(i+1))){
+                    pos = i;
+                }
+            }
+
+            if(pos > -1){
+                equation = replace(equation, findRange(equation, pos), solve(findSet(equation, pos), equation.charAt(i)));
+                if(!p)
+                    System.out.println(equation);
+                i = 0;
+            }
+
+            pos = -1;
+        }
+
+        // ADDITION / SUBTRACTION
+        for(int i = 1; i < equation.length(); i++){
+            if(equation.charAt(i) == '+' || equation.charAt(i) == '-'){
+                pos = i;
+            }
+
+            if(pos > -1){
+                equation = replace(equation, findRange(equation, pos), solve(findSet(equation, pos), equation.charAt(i)));
+                if(!p)
+                    System.out.println(equation);
+                i = 0;
+            }
+
+            pos = -1;
+        }
+
+        return equation;
     }
 
     public static String replace(String equation, int[] range, int result){
@@ -85,17 +136,29 @@ public class Calculator {
         return buildEquation.toString();
     }
 
-    public static int solve(String set){
+    public static String replace(String equation, int[] range, String value){
+        StringBuilder buildEquation = new StringBuilder();
+
+        buildEquation.append(equation.substring(0, range[0]));
+        //System.out.println(buildEquation.toString());
+        buildEquation.append(value);
+        //System.out.println(buildEquation.toString());
+
+        if(equation.length() > range[1]+1)
+            buildEquation.append(equation.substring(range[1]+1, equation.length()));
+
+        return buildEquation.toString();
+    }
+
+    public static int solve(String set, char operation){
         int num1End = -1, num2Start = -1;
         int num1, num2;
         int result;
-        char operation = '&';
         int opPos = -1;
 
         for(int i = 0; i < set.length(); i++){
             for(int j = 0; j < ops.length; j++){
                 if(set.charAt(i) == ops[j]){
-                    operation = ops[j];
                     opPos = i;
                 }
             }
@@ -133,6 +196,12 @@ public class Calculator {
                 break;
             case('-'):
                 result = num1 - num2;
+                break;
+            case('('):
+                result = num1 * num2;
+                break;
+            case(')'):
+                result = num1 * num2;
                 break;
             default:
                 result = num1 + num2;
@@ -175,7 +244,7 @@ public class Calculator {
         for(int i = pos; i >= 0; i--){
             if(!found && Character.isDigit(equation.charAt(i))){
                 found = true;
-            }else if(found && !Character.isDigit(equation.charAt(i)) && !((equation.charAt(i) == '-') && i==0)){
+            }else if(found && !Character.isDigit(equation.charAt(i)) && !((equation.charAt(i) == '-') && i==0) && equation.charAt(i) != '('){
                 start = i+1;
                 i = -100;
             }
@@ -185,7 +254,7 @@ public class Calculator {
         for(int i = pos; i < equation.length(); i++){
             if(!found && Character.isDigit(equation.charAt(i))){
                 found = true;
-            }else if(found && !Character.isDigit(equation.charAt(i))){
+            }else if(found && !Character.isDigit(equation.charAt(i)) && equation.charAt(i) != ')'){
                 end = i-1;
                 i = equation.length()+100;
             }
